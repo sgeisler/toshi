df2ba30981bb6b9db6905958c1ab007d2764a225
Saso Matejina <saso.matejina@coinbase.com>
2014-09-18 14:22:03 -0700

    Api updates, small fixes
    - added search endpoint
    - fixed peers number display
    
    Signed-off-by: Saso Matejina <saso@coinbase.com>

--- lib/toshi/web/api.rb
+++ lib/toshi/web/api.rb
@@ -177,6 +177,50 @@ module Toshi
       end
 
       ####
+      ## /search
+      ####
+      get '/search/:query.?:format?' do
+        # block || tx
+        if params[:query].bytesize == 64
+          if @block = Toshi::Models::Block.where(hsh: params[:query], branch: 0).first
+            path = 'blocks'
+            hash = @block.hsh
+          else
+            if @transaction = Toshi::Models::Transaction.where(hsh: params[:query]).first
+              path = 'transactions'
+              hash = @transaction.hsh
+            end
+          end
+
+        # block height
+        elsif /\A[0-9]+\Z/.match(params[:query])
+          if @block = Toshi::Models::Block.where(height: params[:query].to_i, branch: 0).first
+            path = 'blocks'
+            hash = @block.hsh
+          end
+
+        # address hash
+        elsif Bitcoin.valid_address?(params[:query])
+          if @address = Toshi::Models::Address.where(address: params[:query]).first
+            path = 'addresses'
+            hash = @address.address
+          end
+        end
+
+        raise NotFoundError unless (path && hash)
+
+        case format
+        when 'json'
+          json({
+            path: path,
+            hash: hash
+          })
+        else
+          raise InvalidFormatError
+        end
+      end
+
+      ####
       ## /toshi
       ####
 
