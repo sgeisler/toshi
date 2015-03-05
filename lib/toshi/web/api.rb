a3e5c6bee520994f179a2285b87c46e0627c1273
Maksim Stepanenko <maksimus16@gmail.com>
2015-03-04 16:45:23 -0800

    Add time to the json response

--- lib/toshi/web/api.rb
+++ lib/toshi/web/api.rb
@@ -199,9 +199,10 @@ module Toshi
 
         case format
         when 'json'
-          raise BadRequestError unless params[:time] && params[:time].is_a?(Integer)
-
-          { balance: @address.balance_at(params[:time]) }.to_json
+          {
+            balance: @address.balance_at(params[:time].to_i),
+            time: Time.at(params[:time].to_i).utc
+          }.to_json
         else
           raise InvalidFormatError
         end
