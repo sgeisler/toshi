c51aac20226fd66dfc7b16f567e5b40647832a9d
Sebastian Geisler <sgeisler@wh2.tu-dresden.de>
2016-09-06 02:27:05 +0200

    make bind address configurable
    * use bind instead of port in config
    * set 'tcp://0.0.0.0:5000' as default (preserves defaults)

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,3 @@
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['PUMA_MIN_THREADS']  || 1), Integer(ENV['PUMA_MAX_THREADS'] || 16)
-port ENV['PORT'] || 5000
+bind ENV['BIND'] || 'tcp://0.0.0.0:5000'
